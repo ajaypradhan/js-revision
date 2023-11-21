27c89586c978521e580be95b4fbcaf98322da9a6
Ajay Pradhan <pradhanajay14@gmail.com>
2023-11-21 17:27:49 +0000

    operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = "90"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score)
 
-console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber);
 
 
 // "33" => 33
@@ -17,6 +17,44 @@ let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0=> false; "ajay" => true; "" => false
+
+
+
+// *********************************************OPERATION**************************************************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); //raise to the power
+// console.log(2/2);
+// console.log(2%2); //reminder
+
+
+let srt1 = "hello"
+let srt2 = " ajay"
+let str3 = srt1 + srt2
+// console.log(str3);
+// 
+// console.log("1" + 2); //=>12
+// console.log(1 + "2"); //=> 12
+// console.log("1" + 2 +2); //=> 122
+// console.log(1 + 2 + "2"); //=> 322
+// 
+// console.log(+true); //=> 1
+// console.log(+""); //=> 0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+
+let gameCounter = 101
+gameCounter++
+console.log(gameCounter);

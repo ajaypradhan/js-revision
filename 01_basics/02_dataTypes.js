6474e80aa4de7f229d75f9a1d02b00ad68c216ba
Ajay Pradhan <pradhanajay14@gmail.com>
2023-11-21 16:25:03 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using node, not browser
+
+console.log(3
+    +
+    3);// code readability high proirity
+
+
+console.log("Ajay");
+
+
+let name = "Ajay"
+let age = 23
+let isLoggedIn = false
+
+/* 
+number => 2 to power 53
+bigint
+string => ""
+boolean => true/false
+null => standalone value
+undefined
+symbol => unique
+
+object
+*/
+
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object

60d9ebbbc4f6017df893e79393e8822d5bdc08ba
Ajay Pradhan <pradhanajay14@gmail.com>
2023-11-21 17:36:22 +0000

    comparion

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,18 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" >1);
+// console.log("02" >1);
+
+// avoid below conversion
+console.log(null > 0);
+console.log(null >= 0);
+console.log(null == 0);
+
+
+// === strict check, check without converision
